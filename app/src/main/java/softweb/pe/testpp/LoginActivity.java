1c6da6e2f6b82ab7b662de205c5203d6727a6684
Pepe Valdivia <jvaldivia@softweb.pe>
2017-05-31 18:59:12 -0500

    Avance del login, ahora a cambiar a fragmanents

--- app/src/main/java/softweb/pe/testpp/LoginActivity.java
+++ app/src/main/java/softweb/pe/testpp/LoginActivity.java
@@ -2,8 +2,13 @@ package softweb.pe.testpp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnIngresar;
+    private EditText txtUsuario;
+    private EditText txtContrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
