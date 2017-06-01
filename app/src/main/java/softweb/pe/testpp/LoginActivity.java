09852a08fd0c6ab62b5bc800a99f2bd50f422d43
Pepe Valdivia <jvaldivia@softweb.pe>
2017-06-01 12:56:58 -0500

    Mensaje coloreado de respueta del login en textView

--- app/src/main/java/softweb/pe/testpp/LoginActivity.java
+++ app/src/main/java/softweb/pe/testpp/LoginActivity.java
@@ -1,5 +1,6 @@
 package softweb.pe.testpp;
 
+import android.graphics.Color;
 import android.os.StrictMode;
 import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
 
 import utils.Constants;
 import utils.Httparty;
@@ -16,6 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnIngresar;
     private EditText txtUsuario;
     private EditText txtContrasenia;
+    private TextView lblMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +28,30 @@ public class LoginActivity extends AppCompatActivity {
         this.btnIngresar = (Button) findViewById(R.id.btnIngresar);
         this.txtUsuario = (EditText) findViewById(R.id.txtUsuario);
         this.txtContrasenia = (EditText) findViewById(R.id.txtContrasenia);
+        this.lblMensaje = (TextView) findViewById(R.id.lblMensaje);
 
         this.btnIngresar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String usuario = txtUsuario.getText().toString();
                 String contrasenia = txtContrasenia.getText().toString();
                 String urlLogin = Constants.BASE_URL + "usuario/validar?usuario=" + usuario + "&contrasenia=" + contrasenia;
+                String rpta;
 
                 StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
                 StrictMode.setThreadPolicy(policy);
-                String rpta;
 
                 try{
                     Httparty httpartyLogin = new Httparty(urlLogin, "POST");
                     httpartyLogin.action();
                     rpta = httpartyLogin.getRpta();
+
+                    if(httpartyLogin.getRpta().equalsIgnoreCase("0")){
+                        lblMensaje.setText("El usuario y/o contraseña no son correctos");
+                        lblMensaje.setTextColor(getResources().getColor(R.color.rojoQuinua));
+                    }else if(httpartyLogin.getRpta().equalsIgnoreCase("1")){
+                        lblMensaje.setText("OK... =)");
+                        lblMensaje.setTextColor(getResources().getColor(R.color.verdeQuinua));
+                    }
                 }catch (Exception e){
                     rpta = e.toString();
                 }
