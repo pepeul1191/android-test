0e861e3a6ffe4b056b396b7a4978baf6b6f954f3
Pepe Valdivia <jvaldivia@softweb.pe>
2017-06-01 11:47:03 -0500

    ActivityLogin recive los datos del form

--- app/src/main/java/softweb/pe/testpp/LoginActivity.java
+++ app/src/main/java/softweb/pe/testpp/LoginActivity.java
@@ -2,6 +2,8 @@ package softweb.pe.testpp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
 
@@ -14,5 +16,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.btnIngresar = (Button) findViewById(R.id.btnIngresar);
+        this.txtUsuario = (EditText) findViewById(R.id.txtUsuario);
+        this.txtContrasenia = (EditText) findViewById(R.id.txtContrasenia);
+
+        this.btnIngresar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String usuario = txtUsuario.getText().toString();
+                String contrasenia = txtContrasenia.getText().toString();
+
+                Log.d("MyApp", "usuario : " + usuario + " - contrasenia : " + contrasenia);
+            }
+        });
     }
 }
